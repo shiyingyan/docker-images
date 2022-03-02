bd0d51338ebf56bfcbf8049e37a2839693564158
yansy10 <649239538@qq.com>
2022-03-02 17:13:11 +0800

    增加influxdb

---
+++ influxdb/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+mkdir -p /sany
+mkdir -p  /docker-entrypoint-initdb.d
+cat /sany/influxdb.sh | sed "s/\$db_name/$DB_NAME/g" > /docker-entrypoint-initdb.d/influxdb_new.sh
+cat /sany/influxdb.conf | sed "s/\$db_name/$DB_NAME/g" > /etc/influxdb/influxdb.conf
+
+
+nohup influxd &
+        
+INIT_QUERY="show databases"
+INFLUXDB_INIT_PORT=58086
+INFLUX_USER="root"
+INFLUX_PASSWORD="root"
+INFLUX_CMD="influx -host 127.0.0.1 -port $INFLUXDB_INIT_PORT -username $INFLUX_USER -password $INFLUX_PASSWORD -execute "
+
+        for i in {50..0}; do
+                if $INFLUX_CMD "$INIT_QUERY" &> /dev/null; then
+                        break
+                fi
+                echo 'influxdb init process in progress...'
+                sleep 1
+        done
+
+        if [ "$i" = 0 ]; then
+                echo >&2 'influxdb init process failed.'
+                exit 1
+        fi
+
+        for f in /docker-entrypoint-initdb.d/*; do
+                case "$f" in
+                        *.sh)     echo "$0: running $f"; . "$f" ;;
+                        *.iql)    echo "$0: running $f"; $INFLUX_CMD "$(cat ""$f"")"; echo ;;
+                        *)        echo "$0: ignoring $f" ;;
+                esac
+                echo
+        done
+
+
+#if [ "${1:0:1}" = '-' ]; then
+#    set -- influxd "$@"
+#fi
+
+#/init-influxdb.sh "${@:2}"
+#if [ "$1" = 'influxd' ]; then
+#        /init-influxdb.sh "${@:2}"
+#fi
+
+
+#exec "$@"
+while true; do sleep 10000000; done;
---
+++ influxdb/influxdb.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+databases=$(influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'show databases')
+
+if ( [[ -z $databases ]] || [[ ! $databases =~ $db_name ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE DATABASE "$db_name"'
+fi
+
+if ( [[ -z $databases ]] || [[ ! $databases =~ telegraf ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE DATABASE "telegraf"'
+fi
+
+
+retention_policies=$(influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'show retention policies on $db_name')
+if ( [[ -z $retention_policies ]] || [[ ! $retention_policies =~ rp_7d ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE RETENTION POLICY "rp_7d" ON "$db_name" DURATION 7d REPLICATION 1'
+fi
+
+
+if ( [[ -z $retention_policies ]] || [[ ! $retention_policies =~ rp_30d ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE RETENTION POLICY "rp_30d" ON "$db_name" DURATION 30d REPLICATION 1'
+fi
+
+
+if ( [[ -z $retention_policies ]] || [[ ! $retention_policies =~ rp_ems_60d ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE RETENTION POLICY "rp_ems_60d" ON "$db_name" DURATION inf REPLICATION 1'
+else
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'ALTER RETENTION POLICY "rp_ems_60d" ON "$db_name" DURATION inf REPLICATION 1'
+fi
+
+
+retention_policies=$(influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'show retention policies on telegraf')
+if ( [[ -z $retention_policies ]] || [[ ! $retention_policies =~ autogen ]] ) then
+  influx -host 127.0.0.1 -port 58086 -username 'root' -password 'root'  -execute 'CREATE RETENTION POLICY "autogen" ON "telegraf" DURATION 30d REPLICATION 1'
+fi
