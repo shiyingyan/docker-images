6b561a8b788284f47fdb8a553f41e27639eea335
yansy10 <649239538@qq.com>
2022-06-09 10:20:45 +0800

    修改init组件

--- initalization/run_main.sh
+++ initalization/run_main.sh
@@ -1,6 +1,10 @@
 num=0
 while true;do
 	chown -R 3188:3166 /data /logs;
+	if [ -e /var/run/docker.sock ]; then
+	  chmod 0777 /var/run/docker.sock
+	fi
+
 	if [ $num -lt 6000 ]; then
 	   num=$((num+1));
 	   echo $num;
