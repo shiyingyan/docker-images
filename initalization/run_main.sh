7d2430ceccc09cdb9f65f8ec6bfd91f4cb05f009
yansy10 <649239538@qq.com>
2022-07-12 14:46:09 +0800

    1.修改initialization 脚本； 2. 增加mysql管理

--- initalization/run_main.sh
+++ initalization/run_main.sh
@@ -1,14 +1,15 @@
 num=0
 while true;do
-	chown 3188:3166 /data /logs;
 	if [ -e /var/run/docker.sock ]; then
 	  chmod 0777 /var/run/docker.sock
 	fi
 
 	if [ $num -lt 60000 ]; then
+	   chown -R 3188:3166 /data /logs;
 	   num=$((num+1));
 	   echo $num;
-        else
+  else
+	   chown 3188:3166 /data /logs;
 	   sleep 1m;
 	fi;
-   done;
+done;
